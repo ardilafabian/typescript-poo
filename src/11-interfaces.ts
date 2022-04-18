5f43f43430e9f1566cf4c4f7d3a193a06e8d4dec
Fabian Ardila <fabianra97@gmail.com>
2022-04-17 20:56:59 -0500

    POO interfaces

---
+++ src/11-interfaces.ts
@@ -0,0 +1,52 @@
+export interface Driver {
+    database: string;
+    password: string;
+    port: number;
+
+    connect(): void;
+    disconnect(): void;
+    isConnected(name: string): boolean;
+
+}
+
+// const driver: Driver = {
+//     database: '',
+//     password: '',
+//     port: 123
+// }
+
+class PostgresDriver implements Driver {
+    constructor(
+        public database: string,
+        public password: string,
+        public port: number
+    ){}
+    disconnect(): void {
+        throw new Error("Method not implemented.");
+    }
+    isConnected(name: string): boolean {
+        throw new Error("Method not implemented.");
+    }
+
+    connect(): void {
+        // code
+    }
+}
+
+class OracleDriver implements Driver {
+    constructor(
+        public database: string,
+        public password: string,
+        public port: number
+    ){}
+    disconnect(): void {
+        throw new Error("Method not implemented.");
+    }
+    isConnected(name: string): boolean {
+        throw new Error("Method not implemented.");
+    }
+
+    connect(): void {
+        // code
+    }
+}
