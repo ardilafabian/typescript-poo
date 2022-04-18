6e78d056bbf359a0c0baf8a0664498cbafec61f7
Fabian Ardila <fabianra97@gmail.com>
2022-04-17 21:05:47 -0500

    abstract classes

---
+++ src/12-abstract.ts
@@ -0,0 +1,8 @@
+import { Animal, Dog } from "./09-protected";
+
+// const animal = new Animal('elite');
+// animal.greeting();
+
+const benji = new Dog('benji', 'fabi');
+benji.greeting();
+benji.woof(2);
