9b65fb3da2e58fe7a891541c6ce64c66f24bcaf6
Fabian Ardila <fabianra97@gmail.com>
2022-04-17 19:53:27 -0500

    protected access on classes

---
+++ src/09-protected.ts
@@ -0,0 +1,39 @@
+export class Animal {
+    constructor(protected name: string) {}
+
+    move () {
+        console.log('Moving along!')
+    }
+
+    greeting() {
+        return `Hello, I'm ${this.name}`;
+    }
+
+    protected doSomething() {
+        console.log('doooo');
+    }
+}
+
+export class Dog extends Animal{
+    constructor(name: string, public owner: string) {
+        super(name);
+    }
+
+    woof(times: number) {
+        for (let i = 0; i < times; i++) {
+            console.log(`woof!! ${this.name}`);
+        }
+        this.doSomething();
+    }
+
+    move() {
+        // code
+        console.log('moving as a dog');
+        super.move();
+    }
+}
+
+const benji = new Dog('Benji', 'Fabi');
+// benji.name = 'otro nombre';
+benji.woof(3);
+benji.move();
